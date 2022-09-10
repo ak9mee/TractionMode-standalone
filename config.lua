faf2023db7a2c0a6d733d126287e0e5317cfc0f9
ak9mee <112445400+ak9mee@users.noreply.github.com>
2022-09-09 19:29:34 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,30 @@
+Config = {}
+
+Config.TCon = true --- Enable/Disable Traction Control
+
+Config.TCkey = 20 --- Traction Control Key (Default Z) // https://docs.fivem.net/game-references/controls/
+
+Config.Maxspeed = 34.0 --- In meters per second 34 = 75mph
+
+Config.Minspeed = 0.1 --- In meters per second 0.1 = 0.2mph
+
+Config.action = 0.2     --- How much the TC wil try to stop you sliding Lower = more help
+
+Config.OnScreenTextTC = true --- Enable/Disable On-Screen text
+
+Config.OnScreendisplayTC = true --- Enable/Disable On-Screen display/colours
+
+Config.SimpleTCimmage = true --- a small symbol 
+
+Config.Watermark = false --- Enable/Disable Watermark
+
+Config.Watermark2 = false --- Enable/Disable Watermark 
+
+Config.enableTCtoggle = true
+
+ UITC = {
+
+	x =  0.82 ,	-- Traction Control Screen Coords 	0.0-1.0 left to right 
+	y = 0.720 ,	-- Traction Control Screen Coords 	0.0-1.0 = top to bottom
+
+}
